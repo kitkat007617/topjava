9301679ce6322598c10c41fcf44078596b016476
Nikita <n.klimkin99@yandex.ru>
2021-02-10 18:46:51 +0700

    Add loger to MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 
@@ -9,12 +10,17 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @WebServlet(name = "MealServlet", value = "/MealServlet")
 public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<MealTo> mealsWithExcess = MealsUtil.filteredByStreams(MealsUtil.meals, null, null, 2000);
         request.setAttribute("mealsWithExcess", mealsWithExcess);
+        log.debug("forward to meals");
         RequestDispatcher dispatcher = request.getRequestDispatcher("/meals.jsp");
         dispatcher.forward(request, response);
     }
