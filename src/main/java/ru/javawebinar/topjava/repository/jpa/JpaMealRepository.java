75ecc823aa3e9ba3740e2d8338494dbe091c1fbb
Nikita <n.klimkin99@yandex.ru>
2021-02-28 15:02:28 +0700

    Implement save and delete method in JpaMealRep

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,23 +1,39 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew()) {
+            em.persist(meal);
+            return meal;
+        }
+        return em.merge(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        Query query = em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId");
+        query.setParameter("id", id);
+        query.setParameter("userId", userId);
+        return query.executeUpdate() != 0;
     }
 
     @Override
